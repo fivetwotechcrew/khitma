52a21f108901625aebe429245246c559dda19da7
Nur Hanis Ahmad Tarmizi <67618737+hanistarmizi@users.noreply.github.com>
2026-06-29 14:58:52 +0800

    UI fix
    - changed pages to page(s) for grammatical accuracy
    - added check/uncheck function to the checkboxes in the room main page
    - added check to ensure all boxes are checked before starting a session

--- src/app/components/room/room.ts
+++ src/app/components/room/room.ts
@@ -16,8 +16,20 @@ export class Room implements OnInit {
   room: any = null;
   roomName: string | null = null;
   roomCode: string | null = null;
+  uncheckedPng = "approve.png";
+  checkedPng = "check-box.png";
+  checkPng1 = this.uncheckedPng;
+  checkPng2 = this.uncheckedPng;
+  checkPng3 = this.uncheckedPng;
+  allChecked = false;
+  clicked = false;
 
   roomNameSignal = signal<string | null>(this.roomName);
+  checkPngSignal1 = signal<string | null>(this.checkPng1);
+  checkPngSignal2 = signal<string | null>(this.checkPng2);
+  checkPngSignal3 = signal<string | null>(this.checkPng3);
+  allCheckedSignal = signal<boolean>(this.allChecked);
+  clickedSignal = signal<boolean>(this.clicked);
 
   constructor(
     private readonly supabaseService: SupabaseService,
@@ -40,74 +52,102 @@ export class Room implements OnInit {
   }
 
   async startSession() {
-    try {
-      // get current active khitma
-      const { data, error } = await this.supabaseService.getActiveKhitmaByRoomId(this.room.Id);
-      this.khitma = data && data.length > 0 ? data[0] : null;
-
-      if (this.khitma) { // active khitma exists, continue existing khitma
-        // save khitma ID in cache
-        localStorage.setItem('KhitmaID', this.khitma.Id);
-        localStorage.setItem('PagesReadThisSession', '0');
-
-        // get latest page assignment
-        const { data, error } = await this.supabaseService.getLatestPageAssignment(this.khitma.Id);
-        let lastAssignment = data && data.length > 0 ? data[0] : null;
-        if (lastAssignment) { // get latest page assignment
-          if (lastAssignment.PageNum == 604) { // start new khitma, last khitma is done
-            const { data, error } = await this.supabaseService.createNewKhitma(this.room.Id);
-            this.khitma = data && data.length > 0 ? data[0] : null;
-            console.log(this.khitma);
-
-            if (this.khitma) {
-              this.pageToRead = 1
-              localStorage.setItem('PageToRead', this.pageToRead.toString());
-              localStorage.setItem('KhitmaID', this.khitma.Id);
-              localStorage.setItem('PagesReadThisSession', '0');
-              await this.supabaseService.createNewPageAssignment(this.khitma.Id, this.pageToRead);
-
-              this.router.navigate(['/read']);
-            }
-            else {
-              console.error('Failed to start session.');
-            }
-          }
-          else { // continue current khitma
-            this.pageToRead = lastAssignment.PageNum + 1;
-            await this.supabaseService.createNewPageAssignment(this.khitma.Id, this.pageToRead);
-          }
-        }
-        else { // no pages read for this khitma, start with page 1
-          this.pageToRead = 1;
-          await this.supabaseService.createNewPageAssignment(this.khitma.Id, this.pageToRead);
-        }
-
-        localStorage.setItem('PageToRead', this.pageToRead.toString());
-        this.router.navigate(['/read']);
-      }
-      else { // active khitma does not exist, start new khitma and page assignment
-        const { data, error } = await this.supabaseService.createNewKhitma(this.room.Id);
+    this.clicked = true;
+    this.clickedSignal.set(true);
+    if (this.allChecked) {
+      try {
+        // get current active khitma
+        const { data, error } = await this.supabaseService.getActiveKhitmaByRoomId(this.room.Id);
         this.khitma = data && data.length > 0 ? data[0] : null;
-        console.log(this.khitma);
 
-        if (this.khitma) {
-          this.pageToRead = 1
-          localStorage.setItem('PageToRead', this.pageToRead.toString());
+        if (this.khitma) { // active khitma exists, continue existing khitma
+          // save khitma ID in cache
           localStorage.setItem('KhitmaID', this.khitma.Id);
           localStorage.setItem('PagesReadThisSession', '0');
-          await this.supabaseService.createNewPageAssignment(this.khitma.Id, this.pageToRead);
 
+          // get latest page assignment
+          const { data, error } = await this.supabaseService.getLatestPageAssignment(this.khitma.Id);
+          let lastAssignment = data && data.length > 0 ? data[0] : null;
+          if (lastAssignment) { // get latest page assignment
+            if (lastAssignment.PageNum == 604) { // start new khitma, last khitma is done
+              const { data, error } = await this.supabaseService.createNewKhitma(this.room.Id);
+              this.khitma = data && data.length > 0 ? data[0] : null;
+              console.log(this.khitma);
+
+              if (this.khitma) {
+                this.pageToRead = 1
+                localStorage.setItem('PageToRead', this.pageToRead.toString());
+                localStorage.setItem('KhitmaID', this.khitma.Id);
+                localStorage.setItem('PagesReadThisSession', '0');
+                await this.supabaseService.createNewPageAssignment(this.khitma.Id, this.pageToRead);
+
+                this.router.navigate(['/read']);
+              }
+              else {
+                console.error('Failed to start session.');
+              }
+            }
+            else { // continue current khitma
+              this.pageToRead = lastAssignment.PageNum + 1;
+              await this.supabaseService.createNewPageAssignment(this.khitma.Id, this.pageToRead);
+            }
+          }
+          else { // no pages read for this khitma, start with page 1
+            this.pageToRead = 1;
+            await this.supabaseService.createNewPageAssignment(this.khitma.Id, this.pageToRead);
+          }
+
+          localStorage.setItem('PageToRead', this.pageToRead.toString());
           this.router.navigate(['/read']);
         }
-        else {
-          console.error('Failed to start session.');
+        else { // active khitma does not exist, start new khitma and page assignment
+          const { data, error } = await this.supabaseService.createNewKhitma(this.room.Id);
+          this.khitma = data && data.length > 0 ? data[0] : null;
+          console.log(this.khitma);
+
+          if (this.khitma) {
+            this.pageToRead = 1
+            localStorage.setItem('PageToRead', this.pageToRead.toString());
+            localStorage.setItem('KhitmaID', this.khitma.Id);
+            localStorage.setItem('PagesReadThisSession', '0');
+            await this.supabaseService.createNewPageAssignment(this.khitma.Id, this.pageToRead);
+
+            this.router.navigate(['/read']);
+          }
+          else {
+            console.error('Failed to start session.');
+          }
         }
       }
-    }
-    catch (e) {
-      console.error('Failed to start session.');
+      catch (e) {
+        console.error('Failed to start session.');
+      }
     }
   }
 
+  toggleCheck(checkLabel: string) {
+    switch (checkLabel) {
+      case "1":
+        if (this.checkPng1 == this.uncheckedPng) this.checkPng1 = this.checkedPng;
+        else if (this.checkPng1 == this.checkedPng) this.checkPng1 = this.uncheckedPng;
+        this.checkPngSignal1.set(this.checkPng1);
+        break;
+      case "2":
+        if (this.checkPng2 == this.uncheckedPng) this.checkPng2 = this.checkedPng;
+        else if (this.checkPng2 == this.checkedPng) this.checkPng2 = this.uncheckedPng;
+        this.checkPngSignal2.set(this.checkPng2);
+        break;
+      case "3":
+        if (this.checkPng3 == this.uncheckedPng) this.checkPng3 = this.checkedPng;
+        else if (this.checkPng3 == this.checkedPng) this.checkPng3 = this.uncheckedPng;
+        this.checkPngSignal3.set(this.checkPng3);
+        break;
+      default:
+        break;
+    }
 
+    if (this.checkPng1 == this.checkedPng && this.checkPng2 == this.checkedPng && this.checkPng3 == this.checkedPng) this.allChecked = true;
+    else this.allChecked = false;
+    this.allCheckedSignal.set(this.allChecked);
+  }
 }

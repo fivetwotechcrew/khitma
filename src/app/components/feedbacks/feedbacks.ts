f271f79ba28cffbcb39efab3f42c993f14599f6b
Nur Hanis Ahmad Tarmizi <67618737+hanistarmizi@users.noreply.github.com>
2026-07-07 11:57:49 +0800

    Add back button in feedbacks page

--- src/app/components/feedbacks/feedbacks.ts
+++ src/app/components/feedbacks/feedbacks.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, signal } from "@angular/core";
+import { Component, inject, OnInit, signal } from "@angular/core";
 import { SupabaseService } from '../../services/supabase.service';
 import { DatePipe, NgClass } from "@angular/common";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-feedbacks",
@@ -9,6 +10,7 @@ import { DatePipe, NgClass } from "@angular/common";
   styleUrl: "./feedbacks.css",
 })
 export class Feedbacks implements OnInit {
+  private router = inject(Router);
   feedbacks: Array<any> = [];
   feedbacksSignal = signal<Array<any>>(this.feedbacks);
 
@@ -25,5 +27,9 @@ export class Feedbacks implements OnInit {
     const { data, error } = await this.supabaseService.getFeedbacks();
     this.feedbacks = data && data.length > 0 ? data : [];
     this.feedbacksSignal.set(this.feedbacks);
+  }
+
+  redirect(page: string) {
+    this.router.navigate(['/' + page]);
   }
 }
